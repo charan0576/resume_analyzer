2b16d442c3094300226aada947b92b303e6d6361
naga charan <charandonga9010@gmail.com>
2026-01-13 19:00:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -88,4 +88,8 @@ def download_updated_resume():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
